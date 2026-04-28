41e7c45165f1ae57d0fe9c81bc1da8c28cee5b7c
ederleo21 <ederordonez10@gmail.com>
2026-04-27 21:58:01 -0500

    sh2

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-python manage.py migrate --fake-initial
+
+# Intentar migrar. Si falla por tablas existentes, sincronizamos el historial.
+echo "Sincronizando base de datos en Neon..."
+python manage.py migrate --fake-initial || {
+    echo "Tablas existentes detectadas. Sincronizando historial de migraciones..."
+    python manage.py migrate --fake
+}
